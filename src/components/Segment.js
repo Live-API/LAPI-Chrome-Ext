70a8297f03d09fa6b8b2a90586a96297923232d0
pennwu1 <phw.dental@gmail.com>
2017-08-05 14:14:30 -0700

    Commit Segment.js for merge

--- src/components/Segment.js
+++ src/components/Segment.js
@@ -8,8 +8,8 @@ class SegmentExampleRaised extends Component {
     
 
     <Segment raised>
-        <Input placeholder='Please name your prop' type="text" value={this.props.value} onChange={this.props.setValFunc}/>
-        <Button className="propSaveBtn" onClick={this.props.saveFunc}>Save</Button>
+        <Input id='live-API-property-textbox' placeholder='Please name your prop' type="text" onBlur={(e) => this.props.getPropertyName(e)}/>
+        <Button className="propSaveBtn" onClick={() => this.props.saveProperty(this.props.property)}>Save</Button>
     </Segment>
     )
   }
